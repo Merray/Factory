132b721fd123d66b0ac28f5eb9dbd68608f1e97c
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-09-10 16:07:01 +0200

    Class + Repository de RessourceMaterielle : ok

---
+++ java/src/main/java/com/sopra/Factory/model/Salle.java
@@ -0,0 +1,56 @@
+package com.sopra.Factory.model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.sopra.TPVolAngular.model.Reservation;
+import com.sopra.TPVolAngular.model.view.JsonViews;
+
+@Entity
+@DiscriminatorValue("salle")
+public class Salle extends RessourceMaterielle{
+	
+	@Column(name = "nb_personne")
+	private Integer nbPersonne;
+	
+	@OneToMany(mappedBy="salle")
+	//@JsonView(JsonViews.SalleByIdWithCursus.class)
+    private Set<Cursus> cursus;
+	
+	
+
+	
+	
+	
+	public Set<Cursus> getCursus() {
+		return cursus;
+	}
+
+
+	public void setCursus(Set<Cursus> cursus) {
+		this.cursus = cursus;
+	}
+
+
+	public Salle() {
+		super();
+	}
+
+	
+	public Integer getNbPersonne() {
+		return nbPersonne;
+	}
+
+	public void setNbPersonne(Integer nbPersonne) {
+		this.nbPersonne = nbPersonne;
+	}
+	
+	
+	
+
+}
---
+++ java/src/main/java/com/sopra/Factory/model/VideoProjecteur.java
@@ -0,0 +1,35 @@
+package com.sopra.Factory.model;
+
+import java.util.Set;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.sopra.TPVolAngular.model.Reservation;
+import com.sopra.TPVolAngular.model.view.JsonViews;
+
+@Entity
+@DiscriminatorValue("video_projecteur")
+public class VideoProjecteur extends RessourceMaterielle {
+	
+	@OneToMany(mappedBy="video_projecteur")
+	//@JsonView(JsonViews.VideoProjecteurByIdWithCursus.class)
+    private Set<Cursus> cursus;
+	
+	public VideoProjecteur() {
+		super();
+	}
+
+	public Set<Cursus> getCursus() {
+		return cursus;
+	}
+
+	public void setCursus(Set<Cursus> cursus) {
+		this.cursus = cursus;
+	}
+	
+	
+
+}
