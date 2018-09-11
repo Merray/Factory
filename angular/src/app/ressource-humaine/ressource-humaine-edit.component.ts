bef51abf3529150db4d540fbe1dadff939c10f40
Guilitane <jonvivlau@hotmail.fr>
2018-09-11 15:11:12 +0200

    modification du edit-component des ressources humaines pour permettre de s'occuper des classes abstraites

--- angular/src/app/ressource-humaine/ressource-humaine-edit.component.ts
+++ angular/src/app/ressource-humaine/ressource-humaine-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {RessourceHumaine} from '../model/ressource-humaine';
 import {RessourceHumaineService} from '../service/RessourceHumaine/ressource-humaine.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {Stagiaire} from '../model/RessourceHumaineHeritage/stagiaire';
 
 @Component({
   selector: 'app-ressource-humaine-edit',
@@ -11,6 +12,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 export class RessourceHumaineEditComponent implements OnInit {
 
   ressourceHumaine: RessourceHumaine;
+  stagiaire: Stagiaire;
 
   constructor(private ressourceHumaineService: RessourceHumaineService, private ar: ActivatedRoute, private router: Router) {
   }
@@ -25,6 +27,18 @@ export class RessourceHumaineEditComponent implements OnInit {
     });
   }
 
-  public save() {
+  public save(type: string) {
+    if (type === 'Stagiaire') {
+    // @ts-ignore
+      this.stagiaire = this.ressourceHumaine;
+      this.ressourceHumaineService.save(this.stagiaire).subscribe(resp => {
+        this.router.navigate(['/ressourcehumaine']);
+      });
+    } else {
+      console.log('l\'egalité ne passe pas');
+    }
+    // this.adherentService.save(this.adherent).subscribe(resp => {
+    //  this.router.navigate(['/adherent']);
+    // });
   }
 }
