2d5d13ac122adc3680ac9019a26412a6f0add96d
Guilitane <jonvivlau@hotmail.fr>
2018-09-11 11:09:06 +0200

    modification du typage de prérequis dans le model matière

--- angular/src/app/model/matiere.ts
+++ angular/src/app/model/matiere.ts
@@ -2,6 +2,6 @@ import {Formateur} from './RessourceHumaineHeritage/formateur';
 
 export class Matiere {
   constructor(private titre?: string, private niveau?: string, private duree?: number, private objectifs?: string,
-              private prerequis?: Matiere[], private contenue?: string, private formateurs?: Formateur[]) {
+              private prerequis?: string, private contenue?: string, private formateurs?: Formateur[]) {
   }
 }
