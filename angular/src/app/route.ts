f298e2e6f8163c379978651769db7fc1cae93b6c
Unknown <kim.audoux@gmail.com>
2018-09-11 11:42:38 +0200

    resources mat

--- angular/src/app/route.ts
+++ angular/src/app/route.ts
@@ -1,5 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {RessourceMaterielleComponent} from './ressource-materielle/ressource-materielle.component';
 
 export const routes: Routes = [{path: 'home', component: HomeComponent},
+  {path: 'ressource/materielle', component: RessourceMaterielleComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}];
