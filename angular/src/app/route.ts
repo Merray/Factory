5f7089ad70b027f33a813a0af57f94e843302ab3
Guilitane <jonvivlau@hotmail.fr>
2018-09-11 12:13:49 +0200

    ajout d'une route pour atteindre le component Ressource Humaine

--- angular/src/app/route.ts
+++ angular/src/app/route.ts
@@ -1,7 +1,9 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {RessourceMaterielleComponent} from './ressource-materielle/ressource-materielle.component';
+import {RessourceHumaineComponent} from './ressource-humaine/ressource-humaine.component';
 
 export const routes: Routes = [{path: 'home', component: HomeComponent},
   {path: 'ressource/materielle', component: RessourceMaterielleComponent},
+  {path: 'ressourcehumaine', component: RessourceHumaineComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}];
